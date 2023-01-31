783355a603072703af2afe0ad393134fbd18e48d
Mike Lee <mikemunkyu.lee@gmail.com>
2023-01-30 23:09:49 -0500

    Normalize CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import styles from "@/styles/Home.module.css";
 import { Inter } from "@next/font/google";
 import Head from "next/head";
 
@@ -30,13 +29,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <div
+        className={`flex flex-col my-auto items-center justify-center ${inter.className}`}
+      >
         {currentView === "questions" ? (
           <QuestionsView onShowResults={onShowResults} />
         ) : (
           <ResultsView type={type} onRestart={onRestart} />
         )}
-      </main>
+      </div>
     </>
   );
 }
