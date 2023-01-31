c5d3e7a5439cfe42e55e7536ee837c38829e2170
Mike Lee <mikemunkyu.lee@gmail.com>
2023-01-30 21:25:33 -0500

    Add tailwind

--- src/components/QuestionsView.tsx
+++ src/components/QuestionsView.tsx
@@ -5,20 +5,22 @@ import questionsJSON from "../constants/questions.json";
 export const QuestionsView = () => {
   const questions = useMemo(() => questionsJSON, []);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentQuestion, setCurrentQuestion] = useState(
-    questions[currentIndex]
-  );
-  return (
-    <div>
-      <div>
-        <div>Question {currentIndex + 1}</div>
-        <div>{currentQuestion.question}</div>
 
-        <div className="flex">
-          {currentQuestion.answers.map((answer, index) => {
-            return <button key={index}>{answer.description}</button>;
-          })}
-        </div>
+  const currentQuestion = questions[currentIndex];
+
+  return (
+    <div className="text-lg">
+      <div className="mb-2">Q. {currentIndex + 1}</div>
+      <div className="mb-4">{currentQuestion.question}</div>
+
+      <div className="grid grid-cols-1 gap-4">
+        {currentQuestion.answers.map((answer, index) => {
+          return (
+            <button className="w-80 h-40 bg-slate-100" key={index}>
+              {answer.description}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
